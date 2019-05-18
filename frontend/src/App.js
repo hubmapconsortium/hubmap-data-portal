0395f661dd50d304958ece552b31f0059925a6c2
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-05-18 19:35:34 -0400

    adding organs to svgs with ids and alignment!! TODOs: make organs clickable events and use restful api get request to get data from django

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import human_svg, {ReactComponent as ReactComp} from './images/Human_body_silhouette.svg'
+/* TODO: add clickable events for brain, kidney and lung tissues
+*   TODO: Add api get results fron django to populate results for each tissue
+*    DONE: Added organs prototype!! phew!!*/
 const list = 
 [
 {"id":1,"subclass":{"id":20,"app_label":"dataportal","model":"masscytometrystudy"},"institution":{"id":2,"name":"Vanderbilt"},"data_type":{"id":1,"name":"sc-proteomics"},"tissue":{"id":1,"name":"Brain"},"creation_time":"2019-05-08T19:57:27.788661Z"},
@@ -26,16 +30,17 @@ class App extends Component {
 
  render() {
     return (
-      <div>
-        {this.state.studies.map(item => (
-          <div key={item.subclass.model}>
-            <h1>{item.id}</h1>
-            <span>{item.institution.name}</span>,
-			<span>{item.tissue.name}</span>, 
-			<span>{item.data_type.name}</span> 
-          </div>
-        ))}
-      </div>
+    <div className="App">
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <p>
+          HuBMAP Dataportal!
+        </p>
+      </header>
+        <div className="svgclass">
+        <ReactComp />
+        </div>
+    </div>
     );
   }
 }
