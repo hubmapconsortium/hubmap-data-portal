0037527398b16c8aef752efbaa31f2705a964c1f
Matt Ruffalo <matt.ruffalo@gmail.com>
2019-06-11 15:00:17 -0400

    Add WIP server-rendered human body SVG with dynamic color per tissue

--- hubmap/frontend/views.py
+++ hubmap/frontend/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+import matplotlib.cm
+import numpy as np
+import pandas as pd
 
-# Create your views here.
+def rgba_float_to_rgb_hex(floats):
+    return '#' + ''.join('{:02x}'.format(int(c * 255)) for c in floats[:3])
+
+def tissue_svg(request):
+    # These match the Django template fields in `human_body.svg`
+    organs = [
+        'stomach',
+        'liver',
+        'lung',
+        'small_intestine',
+        'heart',
+        'bladder',
+        'large_intestine',
+        'kidney',
+    ]
+
+    vec = pd.Series(np.random.random(len(organs)), index=organs)
+    hex_values = {
+        f'{tissue}_color': rgba_float_to_rgb_hex(matplotlib.cm.viridis(expr))
+        for tissue, expr in vec.items()
+    }
+    return render(
+        request,
+        'human_body.svg',
+        hex_values,
+        content_type='image/svg+xml',
+    )
