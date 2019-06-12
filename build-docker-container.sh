f33e76cd41c745c13dfe158a3cbdeb4b0d3c3acf
Matt Ruffalo <matt.ruffalo@gmail.com>
2019-06-12 13:04:40 -0400

    Replace build-docker-container.sh with build-docker-container.py

--- build-docker-container.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-TIMESTAMP=$(date +%Y%m%d-%H%M%S%z)
-docker build -t mruffalo/hubmap-data-portal-python:$TIMESTAMP -f docker/uwsgi+python-env/Dockerfile .
-docker build -t mruffalo/hubmap-data-portal-reactjs:$TIMESTAMP -f docker/reactjs_app/Dockerfile .
