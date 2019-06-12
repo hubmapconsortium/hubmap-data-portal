44339183383ea8ee6f31c6d23186acdf0d418522
Matt Ruffalo <matt.ruffalo@gmail.com>
2019-06-12 12:07:14 -0400

    Tweak Docker config for running the ReactJS app as /

--- build-docker-container.sh
+++ build-docker-container.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker build -f docker/uwsgi+python-env/Dockerfile .
+TIMESTAMP=$(date +%Y%m%d-%H%M%S%z)
+docker build -t mruffalo/hubmap-data-portal-python:$TIMESTAMP -f docker/uwsgi+python-env/Dockerfile .
+docker build -t mruffalo/hubmap-data-portal-reactjs:$TIMESTAMP -f docker/reactjs_app/Dockerfile .
