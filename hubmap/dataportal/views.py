34df169e61cb7de458022e242727d412ee605ab9
Matt Ruffalo <matt.ruffalo@gmail.com>
2019-07-19 13:37:06 -0400

    Start to reorganize

--- hubmap/dataportal/views.py
+++ hubmap/dataportal/views.py
@@ -173,25 +173,3 @@ def serialize_multi_dim_counts(data: xr.DataArray):
         ])
 
     return list_for_frontend
-
-class FrontendAppView(View):
-    """
-    Serves the compiled frontend entry point (only works if you have run `yarn
-    run build`).
-    """
-
-    def get(self, request):
-        print(os.path.join(settings.MAIN_PROJECT,'templates', 'index.html'))
-        try:
-            with open(os.path.join(settings.MAIN_PROJECT,'templates', 'index.html')) as f:
-                return HttpResponse(f.read())
-        except FileNotFoundError:
-            logging.exception('Production build of app not found')
-            return HttpResponse(
-                """
-                This URL is only used when you have built the production
-                version of the app. Visit http://localhost:3000/ instead, or
-                run `yarn run build` to test the production version.
-                """,
-                status=501,
-            )
--- hubmap/hubmap/urls.py
+++ hubmap/hubmap/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from dataportal import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('dataportal.urls')),
+    path('', include('frontend.urls')),
 ]
