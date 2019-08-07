198f0bda67f69a7150e7403d6cefd6c5469e5502
Matt Ruffalo <matt.ruffalo@gmail.com>
2019-08-07 12:21:06 -0400

    Remove usage of file that's been deleted in other branches

--- hubmap/dataportal/views.py
+++ hubmap/dataportal/views.py
@@ -9,7 +9,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
-from fixtures.add_tissuegene_heatmaps2db import setcolorheatmap
 from .utils import *
 from .serializers import *
 import matplotlib.cm
