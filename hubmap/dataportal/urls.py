76989b68d2e9f1141719038e7f8a1bcce04af2e9
Matt Ruffalo <matt.ruffalo@gmail.com>
2019-08-12 10:41:09 -0400

    Fix URL pattern

--- hubmap/dataportal/urls.py
+++ hubmap/dataportal/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     path('', views.StudyListView.as_view()),
     path('experiments/', views.StudyListPageView.as_view()),
     path('<int:pk>/', views.StudyDetailView.as_view()),
-    path('search/$', views.GlobalSearch.as_view(), name="search"),
+    path('search/', views.GlobalSearch.as_view(), name="search"),
     path('colors/', views.Tissue_svg_colors.as_view(), name="colors"),
     path('genes/', views.GeneListView.as_view(), name="genes"),
     path('protected/', login_required(views.StudyListView.as_view())),
