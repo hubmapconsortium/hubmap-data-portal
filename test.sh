b3c4fef97e8733d3cc98206ddec27b1c74841db2
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-08-13 15:15:57 -0400

    Add to test.sh

--- test.sh
+++ test.sh
@@ -6,6 +6,17 @@ start() { echo travis_fold':'start:$1; echo $1; }
 end() { set +v; echo travis_fold':'end:$1; echo; echo; }
 die() { set +v; echo "$*" 1>&2 ; exit 1; }
 
+start cypress
+# TODO: Move end-to-end tests to end of the list when stable.
+pushd hubmap/frontend
+npm start &
+SERVER_PID=$!
+$(npm bin)/wait-on http://localhost:3000
+$(npm bin)/cypress run
+kill $SERVER_PID
+popd
+end cypress
+
 start flake8
 pushd hubmap
 flake8
