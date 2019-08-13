e3be4b8158a2fff7ba915b5ce5d81edff86bf1a5
Matt Ruffalo <matt.ruffalo@gmail.com>
2019-08-13 12:23:50 -0400

    apiAdapter.js: don't hardcode production hostname

--- hubmap/frontend/src/commons/apiAdapter.js
+++ hubmap/frontend/src/commons/apiAdapter.js
@@ -2,6 +2,6 @@ let API_URL
 
 process.env.REACT_APP_STAGE === 'dev'
     ? API_URL = 'http://localhost:8000/'
-    : API_URL = 'https://data.dev.hubmapconsortium.org/'
+    : API_URL = '/'
 
 export default API_URL;
