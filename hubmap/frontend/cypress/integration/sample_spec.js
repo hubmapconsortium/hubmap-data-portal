636437f4a3038d232d2d5a4188ee6dc85fab267a
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-08-13 12:37:18 -0400

    Basic tests for content of homepage

--- hubmap/frontend/cypress/integration/sample_spec.js
+++ hubmap/frontend/cypress/integration/sample_spec.js
@@ -1,5 +1,27 @@
+/* eslint no-undef: 0 */
+// TODO: Configure eslint to recognize cy as a global.
 describe('HuBMAP', function() {
   it('Has a homepage', function() {
-    cy.visit('http://localhost:3000')
-  })
+    cy.visit('http://localhost:3000');
+
+    // Header:
+    cy.contains('Browse');
+    cy.contains('Help');
+    cy.contains('Login');
+
+    // Charts:
+    cy.contains('# of Cells per Tissue, by Center');
+    cy.contains('# Images per Tissue, by Center');
+    cy.contains('Caltech');
+    cy.contains('Abdomen');
+
+    // Table:
+    cy.contains('Experiments From HuBMAP Consortium');
+    cy.contains('Imaging Mass Spec.');
+
+    cy.contains('Experiments by Center');
+
+    // Footer:
+    cy.contains('Supported by the NIH Common Fund');
+  });
 })
