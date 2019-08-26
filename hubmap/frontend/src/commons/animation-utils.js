53f2ea66db6f871195a620e063cde69f248831e8
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-08-26 11:41:23 -0400

    review comments animation-utils.js :
    1) Consider using Array.from so you can use forEach
    2) capitalize comment

--- hubmap/frontend/src/commons/animation-utils.js
+++ hubmap/frontend/src/commons/animation-utils.js
@@ -1,16 +1,16 @@
 let dynamicStyles = null;
 export default function addAnimationToStyle(animationName, animationSteps) {
-  // add style
+  // Add style
   if (!dynamicStyles) {
     dynamicStyles = document.createElement('style');
     dynamicStyles.type = 'text/css';
     document.head.appendChild(dynamicStyles);
   }
-  for (let i = 0; i < dynamicStyles.sheet.cssRules.length; i += 1) {
-    if (dynamicStyles.sheet.cssRules[i].name === animationName) {
+  Array.from(dynamicStyles.sheet.cssRules).forEach((rule, i) => {
+    if (rule.name === animationName) {
       dynamicStyles.sheet.deleteRule(i);
     }
-  }
+  });
   dynamicStyles.sheet.insertRule(`@keyframes ${animationName} {
         ${animationSteps}}`, dynamicStyles.length);
 }
