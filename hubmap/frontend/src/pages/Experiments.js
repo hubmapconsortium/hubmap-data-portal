867be09ed6a8b346b6f6d4c432659fe853cb3a59
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-09 19:56:33 -0400

    modify Experiments table to include the Grid containers and components
    1) add all ExperimentsTable, dropdown, search options

--- hubmap/frontend/src/pages/Experiments.js
+++ hubmap/frontend/src/pages/Experiments.js
@@ -1,15 +1,53 @@
-// TODO!
-/* eslint-disable react/prefer-stateless-function */
-
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import grey from '@material-ui/core/colors/grey';
+import Title from '../components/browse/Title';
+import {
+  ExperimentsTable,
+  SearchDropdown,
+  SpecimenDropdown,
+  DonorDropdown,
+} from '../components/browse';
 
-class ExperimentsComponent extends React.Component {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: grey[300],
+    marginTop: '150px',
+  },
+
+}));
+export default class Experiments extends React.Component {
+  componentDidMount() {
+
+  }
+
   render() {
     return (
-      <div style={{ margin: 100 }}>
-Experiments
-      </div>
+        <div className={useStyles.root}>
+        <Grid container spacing={3} style={{marginTop: '150px'}} justify="left" alignItems="left">
+          <Grid item xs={12}>            
+          <Title>Explore Experiments data</Title>
+          </Grid>
+          <Grid item xs={3}  style={{ marginLeft:'-80px' }}>
+          <SearchDropdown />
+          </Grid>
+          <Grid item xs={3} >
+          <DonorDropdown />
+          </Grid>
+          <Grid item xs={3}>
+          <DonorDropdown />
+          </Grid>
+          <Grid item xs={3}>
+          <DonorDropdown />
+          </Grid>
+          <Grid item xs={12}>            
+
+          <ExperimentsTable />
+          </Grid>
+        </Grid>
+        </div>
     );
   }
 }
-export default ExperimentsComponent;
