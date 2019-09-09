668065cb80c73b34ad3bbbecb50c80f39860feab
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-09 19:17:01 -0400

    update renamed component name

--- hubmap/frontend/src/components/RootContainer.js
+++ hubmap/frontend/src/components/RootContainer.js
@@ -5,7 +5,7 @@ import Footer from './Footer';
 import NavBar from './NavBar';
 import DataAnalysisComponent from '../pages/DataAnalysis';
 import UserFAQsComponent from '../pages/UserFAQs';
-import ExperimentsComponent from '../pages/Experiments';
+import Experiments from '../pages/Experiments';
 import PipelinesComponent from '../pages/Pipelines';
 import HomePage from '../pages/HomePage';
 
@@ -14,9 +14,9 @@ function RootContainer() {
     <BrowserRouter>
       <NavBar />
       <Container xl="true" component="div" className="gridContainer" style={{ height: '100vh', flexGrow: 1 }}>
+        <Route exact path="/experiments" component={Experiments} />
         <Route exact path="/dataanalysis" component={DataAnalysisComponent} />
         <Route exact path="/userfaqs" component={UserFAQsComponent} />
-        <Route exact path="/experiments" component={ExperimentsComponent} />
         <Route exact path="/pipelines" component={PipelinesComponent} />
         <Route exact path="/" component={HomePage} />
         <Footer />
