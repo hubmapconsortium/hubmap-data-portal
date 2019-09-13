4b60e541558fc3f25de31ae6abefe7717cbdbfa0
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-13 16:57:21 -0400

    Add child dropdown menu to add as many as menusections props received from props

--- hubmap/frontend/src/ui-components/BaseDropdownPanel.js
+++ hubmap/frontend/src/ui-components/BaseDropdownPanel.js
@@ -5,48 +5,57 @@ import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 import { grey, blueGrey } from '@material-ui/core/colors';
 import PropTypes from 'prop-types';
-import { Checkbox } from '@material-ui/core';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
+import Grid from '@material-ui/core/Grid';
+import BaseChildDropdown from './BaseChildDropdown';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import ElevationScroll from './ElevationScroll';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    position: 'relative',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+  },
+  divroot: {
+    flexGrow: 1,
+    backgroundColor: grey[300],
+    marginTop: '50px',
+    display: 'flex',
+    width:'900px'
   },
   paper: {
-    position: 'absolute',
+    //position: 'absolute',
     top: 36,
     right: 0,
     left: 0,
     backgroundColor: '#eeeeee',
-    marginTop: 30,
+    width: '100%',
+    justifyContent: 'space-between',
   },
   formControl: {
     marginTop: 50,
   },
+  gridList: {
+    width: 'auto',
+    height: 300,
+  },
 }));
 
-const GreyCheckbox = withStyles({
-    root: {
-      color: blueGrey[400],
-      '&$checked': {
-        color: blueGrey[600],
-      },
-    },
-    checked: {},
-  })((props) => <Checkbox color="default" {...props} />);
-
-export default class BaseDropDownPanel extends React.Component {
+export default class BaseDropdownPanel extends React.Component {
   container = React.createRef();
 
   constructor(props) {
     super(props);
     this.state = {
+      menusections: {},
+      menuname:'',
       open: false,
-      selectedMenu: '',
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleClickAway = this.handleClickAway.bind(this);
@@ -60,49 +69,48 @@ export default class BaseDropDownPanel extends React.Component {
       this.setState({ open: false });
     }
 
-    handleChange = (name) => (event) => {
-      const { selectedMenu } = this.state;
-      this.setState({ selectedMenu: event.target.checked });
-    };
-    
-
     render() {
       const { open } = this.state;
-      const { menuitems, menuname } = this.props;
+      const { menusections,menuname } = this.props;
+      const htmlElements = [];
+      let i=0;
+      for (const key in menusections) {
+        console.log(menusections[key], key);
+        htmlElements.push(                
+        <GridListTile key={key} cols={i++}>
+        <BaseChildDropdown
+          menuitems={menusections[key]}
+          menuname={key}
+        />        
+        </GridListTile>
+        );
+      }
       return (
-        <div className={useStyles.root} ref={this.container}>
-          <ClickAwayListener onClickAway={this.handleClickAway}>
-            <div>
-              <Button onClick={this.handleClick}>{menuname}</Button>
-              {open ? (
-                <Paper className={useStyles.paper}>
-                  <FormControl component="fieldset" className={useStyles.formControl}>
-                   <FormLabel component="label" color={grey[400]}>{menuname}</FormLabel>
-                     <FormGroup>
-                  { menuitems ? menuitems.slice(0,menuitems.length).map((menuitem) => {
-                      return <FormControlLabel  key={menuitem}
-                      control={<GreyCheckbox onChange={this.handleChange(menuitem)} value={menuitem}  key={menuitem} />}
-                      label={menuitem}
-                    />
-                  }) : null}
-                  
-                  </FormGroup>
-                <FormHelperText></FormHelperText>
-                </FormControl>
-                </Paper>
-              ) : null}
-            </div>
-          </ClickAwayListener>
-        </div>
+        <ClickAwayListener onClickAway={this.handleClickAway}>
+          <div className={useStyles.root}>
+            <Button onClick={this.handleClick}>{menuname}</Button>
+            {open ? (
+             // <Paper className={useStyles.paper}>
+
+                <GridList cellHeight={"auto"} className={useStyles.gridList} cols={6}>
+                {htmlElements}
+                  <FormHelperText />
+                    </GridList>
+              //</Paper>
+            ) : null}
+          </div>
+        </ClickAwayListener>
       );
     }
 }
 
-BaseDropDownPanel.propTypes = {
-  menuitems: PropTypes.array,
+BaseDropdownPanel.propTypes = {
+  menusections: PropTypes.array,
   menuname: PropTypes.string,
+  open: PropTypes.bool,
 };
-BaseDropDownPanel.defaultProps = {
-  menuitems: {},
+BaseDropdownPanel.defaultProps = {
+  menusections: {},
   menuname: '',
+  open: false,
 };
