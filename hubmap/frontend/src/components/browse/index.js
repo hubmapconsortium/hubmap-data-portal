a06d9c8a7bf1823b1f1e867e998c0bf80160d271
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-16 17:13:55 -0400

    add newly added Dropdown menus to exports in browse/

--- hubmap/frontend/src/components/browse/index.js
+++ hubmap/frontend/src/components/browse/index.js
@@ -6,3 +6,6 @@ export { default as SearchDropdown } from './SearchDropdown';
 export { default as SpecimenDropdown } from './SpecimenDropdown';
 export { default as DonorDropdown } from './DonorDropdown';
 export { default as ExperimentsTable } from './ExperimentsTable';
+export { default as TissueTypeDropdown } from './TissueTypeDropdown';
+export { default as MethodDropdown } from './MethodDropdown';
+export { default as FileDropdown } from './FileDropdown';
