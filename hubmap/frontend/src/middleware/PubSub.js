9586e452a8edfa0f5235a980a90633098d8ba00e
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-17 14:03:50 -0400

    remove additional publishers, subscribers list 1) PubSub-js maintains these lists 2) no need to duplicate mainatining own pubs/subs lists

--- hubmap/frontend/src/middleware/PubSub.js
+++ hubmap/frontend/src/middleware/PubSub.js
@@ -3,29 +3,16 @@ import PubSub from 'pubsub-js';
 export default class PubSubApi {
 tokens = {};
 
-subscribers = [];
-
-publishedTopics = [];
-
 subscribe(topic, subscriber) {
-  this.subscribers.push(subscriber);
   this.tokens[subscriber] = PubSub.subscribe(topic, subscriber);
 }
 
 publish(topic, object) {
-    this.publishedTopics[topic] = object;
-    PubSub.publish(topic, object);
- /*  if (this.publishedTopics.findIndex(topic) !== -1) {
-    PubSub.publish(topic, object);
-  }
-  else {
-    this.publishedTopics[topic] = object;
-    PubSub.publish(topic, object);
-  } */
+  PubSub.publish(topic, object);
 }
 
 unsubscribe(token) {
-    PubSub.unsubscribe(token);
+  PubSub.unsubscribe(token);
 }
 
 }
