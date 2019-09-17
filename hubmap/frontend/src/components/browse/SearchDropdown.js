ad28b47a2e82c170dbb4b8442b76b1ee80edc3a6
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-17 14:03:13 -0400

    Update Search Dropdown to use Controlled chipinput that has custom themel/layout

--- hubmap/frontend/src/components/browse/SearchDropdown.js
+++ hubmap/frontend/src/components/browse/SearchDropdown.js
@@ -1,16 +1,6 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Chip from '@material-ui/core/Chip';
-import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
-import { CustomSearchInput, ControlledChipInput } from '../../ui-components/';
+import { ControlledChipInput } from '../../ui-components';
 
 const useStyles = makeStyles((theme) => ({
   chip: {
