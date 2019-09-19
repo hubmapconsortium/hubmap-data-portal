b6bf6ffd15a26f74123d42969cfaeea5bd1e9e74
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-19 15:36:44 -0400

    add experiments rows and columns headers from json
    1) add column names, rows from experiments.json
    2) adjust column sizes from json content (percentage sizes of overall width)
    3) add styling to column header
    4) add elements from json -> keys and iter

--- hubmap/frontend/src/components/browse/ExperimentsTable.js
+++ hubmap/frontend/src/components/browse/ExperimentsTable.js
@@ -2,13 +2,12 @@
 /* eslint-disable no-script-url */
 
 import React from 'react';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import experiments from '../../data/experiments.json';
 
 // Generate Order Data
 function createData(experimentTitle, sampleType, organ, selectedCellType, libraryConstructionMethod, pairedEnd,
@@ -28,57 +27,52 @@ function createData(experimentTitle, sampleType, organ, selectedCellType, librar
   };
 }
 
-const rows = [
-  createData(
-    'experimentTitle',
-    'sampleType',
-    'organ',
-    'selectedCellType',
-    'libraryConstructionMethod',
-    'pairedEnd',
-    'species',
-    'knownDiseases',
-    'projectDownloads',
-    'donorCount',
-    'cellCountEstimate',
-  ),
-];
-
 export default function ExperimentsTable() {
+  const experimentsDict = experiments.experiments;
+  let columnNames = Object.keys(experimentsDict[1]);
+  let columns = [];
+  var experimentsRows = [];
+  console.log(columnNames, experimentsRows);
+  for ( const i in columnNames){
+    columns.push(<TableCell style={{minWidth: '80px', fontWeight: 'bold', color:'black', fontSize:'10px'}}>{columnNames[i]}</TableCell>);   
+  }
+  for (const key in experimentsDict)
+  {
+      console.log(key, experimentsDict[key], experimentsDict[key][0]);
+      const keys = Object.keys(experimentsDict[key]);
+          experimentsRows.push(
+            <TableRow key={key}>
+              {keys.map((k) =>{
+                return ( <TableCell>{experimentsDict[key][k]} </TableCell>);
+             })}
+
+            </TableRow>
+          );
+      console.log(experimentsRows[0]);
+  }
   return (
     <>
       <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Experiment Title</TableCell>
-            <TableCell>Sample Type</TableCell>
-            <TableCell>Organ/Model Organ</TableCell>
-            <TableCell>Selected Cell Type</TableCell>
-            <TableCell>Library Construction method</TableCell>
-            <TableCell>Paired End</TableCell>
-            <TableCell>Species</TableCell>
-            <TableCell>Known Diseases (Specimen) </TableCell>
-            <TableCell>Project Downloads</TableCell>
-            <TableCell>Donor Count</TableCell>
-            <TableCell>Cell Count Estimate</TableCell>
+        <colgroup>
+          <col style={{width:'2%'}} />
+          <col style={{width:'30%'}} />
+          <col style={{width:'5%'}} />
+          <col style={{width:'5%'}} />
+          <col style={{width:'15%'}} />
+          <col style={{width:'15%'}} />
+          <col style={{width:'10%'}} />
+          <col style={{width:'25%'}} />
+          <col style={{width:'5%'}} />
+          <col style={{width:'5%'}} />
+        </colgroup>
+        <TableHead fixedHeader>
+          <TableRow head>
+            {columns}
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.experimentTitle}>
-              <TableCell>{row.experimentTitle} </TableCell>
-              <TableCell> {row.sampleType}</TableCell>
-              <TableCell> {row.organ}</TableCell>
-              <TableCell>{row.selectedCellType}</TableCell>
-              <TableCell>{row.libraryConstructionMethod}</TableCell>
-              <TableCell>{row.pairedEnd}</TableCell>
-              <TableCell>{row.species}</TableCell>
-              <TableCell>{row.knownDiseases}</TableCell>
-              <TableCell>{row.projectDownloads}</TableCell>
-              <TableCell>{row.donorCount}</TableCell>
-              <TableCell>{row.cellCountEstimate}</TableCell>
-            </TableRow>
-          ))}
+          
+          {experimentsRows}
         </TableBody>
       </Table>
     </>
