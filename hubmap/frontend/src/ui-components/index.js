56c9c949f876e0e24a3bef228e789cbc01224363
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-20 15:24:19 -0400

    remove unused(ui components) components (not used anymore, after templating)
    1) the dropdown panels/Chip components are replaced.
    2) update exports in index.js

--- hubmap/frontend/src/ui-components/index.js
+++ hubmap/frontend/src/ui-components/index.js
@@ -2,12 +2,8 @@
  * This exports all required components for browse data page.
  */
 
-export { default as Chip } from './Chip';
-export { default as DropdownMenu } from './DropdownMenu';
 export { default as CustomSearchInput } from './CustomSearchInput';
 export { default as ControlledChipInput } from './ControlledChipInput';
-export { default as SelectDropdown } from './SelectDropdown';
-export { default as BaseDropdownPanel } from './BaseDropdownPanel';
 export { default as ElevationScroll } from './ElevationScroll';
 export { default as CustomSelectMenu } from './CustomSelectMenu';
 export { default as SelectedBrowseOptionsSummary } from './SelectedBrowseOptionsSummary';
