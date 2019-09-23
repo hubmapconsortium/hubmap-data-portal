e206a60d535599fbc08deb0fa357b5b5f56aa99c
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-09-23 15:01:34 -0400

    Fix lint in apiAdapter.js

--- hubmap/frontend/src/commons/apiAdapter.js
+++ hubmap/frontend/src/commons/apiAdapter.js
@@ -1,10 +1,4 @@
-// TODO!
-/* eslint-disable import/no-mutable-exports */
-/* eslint-disable prefer-const */
-
-let API_URL;
-
-API_URL = process.env.REACT_APP_STAGE === 'dev'
+const API_URL = process.env.REACT_APP_STAGE === 'dev'
   ? 'http://localhost:8000/'
   : '/';
 
