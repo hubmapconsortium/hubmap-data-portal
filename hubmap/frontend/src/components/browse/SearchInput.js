b866b4f63a706c557be51244152a006c33ed37b5
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-24 14:35:43 -0400

    fix eslint errors for SearchInput rename SearchDropdown to SearchInput

---
+++ hubmap/frontend/src/components/browse/SearchInput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ControlledChipInput } from '../../ui-components';
+
+export default function SearchDropdown() {
+  return (
+    <div>
+      <ControlledChipInput />
+    </div>
+  );
+}
