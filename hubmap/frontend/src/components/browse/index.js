e46802f1e0d95460d43c28be8e59ee68b798dc5e
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-24 14:36:15 -0400

    updated renamed file/export

--- hubmap/frontend/src/components/browse/index.js
+++ hubmap/frontend/src/components/browse/index.js
@@ -2,7 +2,7 @@
  * This exports all required components for browse data page.
  */
 
-export { default as SearchDropdown } from './SearchDropdown';
+export { default as SearchInput } from './SearchInput';
 export { default as SpecimenDropdown } from './SpecimenDropdown';
 export { default as DonorDropdown } from './DonorDropdown';
 export { default as ExperimentsTable } from './ExperimentsTable';
