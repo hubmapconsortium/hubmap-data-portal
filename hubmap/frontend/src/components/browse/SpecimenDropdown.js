16e799dac805ccc0f028a20f14c5f6e03f3c6813
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-24 14:29:22 -0400

    fix eslint errors for SpecimenDropdown

--- hubmap/frontend/src/components/browse/SpecimenDropdown.js
+++ hubmap/frontend/src/components/browse/SpecimenDropdown.js
@@ -1,25 +1,16 @@
-import React, { PureComponent } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Chip from '@material-ui/core/Chip';
-import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
+import React from 'react';
 import { CustomSelectMenu } from '../../ui-components';
 import facets from '../../data/searchfacets.json';
 
 export default function SpecimenComponent() {
-    const menuCategories = facets.facets.Specimen;
-  return ( <CustomSelectMenu 
-    margin="normal"
+  const menuCategories = facets.facets.Specimen;
+  return (
+    <CustomSelectMenu
+      margin="normal"
       variant="outlined"
-      menusections={ menuCategories}
+      menusections={menuCategories}
       menuname="Specimen"
-      width='200px'
-    />);
+      width="200px"
+    />
+  );
 }
