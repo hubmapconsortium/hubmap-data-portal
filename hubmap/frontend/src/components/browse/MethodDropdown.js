7cde28438a5f6e7026fc27bd74d819252ca67daf
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-24 14:33:30 -0400

    fix eslint errors for MethodDropdown

--- hubmap/frontend/src/components/browse/MethodDropdown.js
+++ hubmap/frontend/src/components/browse/MethodDropdown.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { CustomSelectMenu } from '../../ui-components';
 import facets from '../../data/searchfacets.json';
-
-const useStyles = makeStyles((theme) => ({
-  chip: {
-    margin: theme.spacing(1),
-  },
-  textField: {
-    //backgroundColor: '#ffffff',
-  },
-}));
 
 export default function DonorDropdown() {
   const menuCategories = facets.facets.Method;
 
-  return (<CustomSelectMenu 
-        margin="normal"
-          variant="outlined"
-          menusections={ menuCategories}
-          menuname="Method"
-          width='200px'
-        />
+  return (
+    <CustomSelectMenu
+      margin="normal"
+      variant="outlined"
+      menusections={menuCategories}
+      menuname="Method"
+      width="200px"
+    />
   );
 }
