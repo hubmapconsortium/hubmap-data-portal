4c18380eb93915c3f6bf434759c02095b0264db8
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-24 14:42:54 -0400

    fix eslint errors for CustomSelectMenu

--- hubmap/frontend/src/ui-components/CustomSelectMenu.js
+++ hubmap/frontend/src/ui-components/CustomSelectMenu.js
@@ -1,5 +1,5 @@
+/* eslint-disable react/no-access-state-in-setstate */
 import React from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 import Select from '@material-ui/core/Select';
 import PropTypes from 'prop-types';
@@ -10,34 +10,6 @@ import BaseChildDropdown from './BaseChildDropdown';
 import { PubSubApi } from '../middleware';
 import * as Commons from '../commons';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    // flexWrap: 'wrap',
-  },
-  formControl: {
-    // margin: theme.spacing(1),
-    width: 200,
-    fullWidth: true,
-    borderColor: grey[500],
-    borderWidth: 1,
-    fontColor: 'black',
-    fontSize: '18px',
-    // display: 'flex',
-    // wrap: 'nowrap',
-  },
-  chips: {
-    display: 'flex',
-    // flexWrap: 'wrap',
-  },
-  chip: {
-    margin: 2,
-  },
-  noLabel: {
-    // marginTop: theme.spacing(3),
-  },
-}));
-  
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 12;
 const MenuProps = {
@@ -82,6 +54,7 @@ theme.overrides = {
     },
   },
 };
+// eslint-disable-next-line prefer-const
 let selectedMenuSummary = [];
 
 export default class CustomSelectMenu extends React.Component {
@@ -98,43 +71,44 @@ export default class CustomSelectMenu extends React.Component {
     this.pubsubObj = new PubSubApi();
   }
 
-  selectedMenuSummaryAdded(msg, summary) {
+  selectedMenuSummaryAdded = (msg, summary) => {
     if (msg === Commons.CHECKED_MENU_OPTIONS
         && selectedMenuSummary.indexOf(summary) === -1) {
       selectedMenuSummary.push(summary);
     }
     if (msg === Commons.UNCHECKED_MENU_OPTIONS
         && selectedMenuSummary.indexOf(summary) === -1) {
-      var index = selectedMenuSummary.indexOf(summary);
-      if ( index > -1 )
-      {
+      const index = selectedMenuSummary.indexOf(summary);
+      if (index > -1) {
         selectedMenuSummary.splice(index, 1);
       }
     }
   }
 
   componentWillMount() {
-    this.menutoken = this.pubsubObj.subscribe(Commons.CHECKED_MENU_OPTIONS, this.selectedMenuSummaryAdded.bind(this));
+    this.menutoken = this.pubsubObj
+      .subscribe(Commons.CHECKED_MENU_OPTIONS, this.selectedMenuSummaryAdded.bind(this));
   }
 
   render() {
     const { menusections, menuname } = this.props;
     const htmlElements = [];
-    const keys = [], values= [];
-    for (const key in menusections) {
-      keys.push(key)
+    const keys = [];
+    const values = [];
+    Object.keys(menusections).forEach((key) => {
+      keys.push(key);
       values.push(menusections[key]);
-      htmlElements.push(             
+      htmlElements.push(
         <BaseChildDropdown
-            menuitems={menusections[key]}
-            menuname={key}
-            selectedMenu={ selectedMenuSummary}
-          />
+          menuitems={menusections[key]}
+          menuname={key}
+          selectedMenu={selectedMenuSummary}
+        />,
       );
-    }
+    });
 
-    return(
-          
+    return (
+
       <MuiThemeProvider theme={theme}>
         <Select
           multiple
@@ -142,10 +116,11 @@ export default class CustomSelectMenu extends React.Component {
           name={menuname}
           onChange={(event) => {
             event.persist();
-            this.setState({ ...this.state, menuitem: event.target.value});
+            this.setState({ ...this.state, menuitem: event.target.value });
           }}
           input={<Input id="select-multiple-placeholder" />}
-          renderValue={(selected) => (this.props.menuname)}
+          // eslint-disable-next-line react/destructuring-assignment
+          renderValue={() => (this.props.menuname)}
           MenuProps={MenuProps}
           variant="outlined"
         >
@@ -162,6 +137,7 @@ CustomSelectMenu.defaultProps = {
   menuname: 'Menu',
 };
 CustomSelectMenu.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
   menusections: PropTypes.array,
   menuname: PropTypes.string,
 };
