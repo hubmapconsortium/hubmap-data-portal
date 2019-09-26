544f7f209c58e474f7859224326353e0c3bf16aa
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-26 16:04:39 -0400

    Add a title for browse page

---
+++ hubmap/frontend/src/components/browse/Title.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+import grey from '@material-ui/core/colors/grey';
+
+export default function Title(props) {
+  const { children } = props;
+  return (
+    <Typography variant="h5" align="left" color={grey[800]} gutterBottom>
+      {children}
+    </Typography>
+  );
+}
+
+Title.propTypes = {
+  children: PropTypes.node,
+};
+
+Title.defaultProps = {
+  children: {},
+};
