f106a454918d261b556cf5949ee283fb51184660
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-26 16:05:18 -0400

    add exports for table and title

---
+++ hubmap/frontend/src/components/browse/index.js
@@ -0,0 +1,6 @@
+/** *
+ * This exports all required components for browse data page.
+ */
+
+export { default as ExperimentsTable } from './ExperimentsTable';
+export { default as Title } from './Title';
