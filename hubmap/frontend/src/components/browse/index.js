8ad10a89178a5b6dbe950101e1b78ed74061b91c
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-26 16:16:15 -0400

    add all exports for dropdown menus

--- hubmap/frontend/src/components/browse/index.js
+++ hubmap/frontend/src/components/browse/index.js
@@ -4,3 +4,9 @@
 
 export { default as ExperimentsTable } from './ExperimentsTable';
 export { default as Title } from './Title';
+export { default as SpecimenDropdown } from './SpecimenDropdown';
+export { default as DonorDropdown } from './DonorDropdown';
+export { default as TissueTypeDropdown } from './TissueTypeDropdown';
+export { default as MethodDropdown } from './MethodDropdown';
+export { default as FileDropdown } from './FileDropdown';
+export { default as AssaysDropdown } from './AssaysDropdown';
