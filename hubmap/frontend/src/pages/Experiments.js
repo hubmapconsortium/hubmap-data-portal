78189675e6f352ac5dd1da72b5c55505936edf02
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-26 16:05:37 -0400

    add title,experiments table in experiments page

--- hubmap/frontend/src/pages/Experiments.js
+++ hubmap/frontend/src/pages/Experiments.js
@@ -1,15 +1,51 @@
-// TODO!
-/* eslint-disable react/prefer-stateless-function */
-
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import red from '@material-ui/core/colors/red';
+import { Divider, Typography } from '@material-ui/core';
+import {
+  ExperimentsTable,
+  Title,
+} from '../components/browse';
 
-class ExperimentsComponent extends React.Component {
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+    marginTop: '150px',
+    width: '2100px',
+    marginLeft: '-200px',
+  },
+
+}));
+export default class Experiments extends React.Component {
+  componentDidMount() {
+
+  }
+
   render() {
     return (
-      <div style={{ margin: 100 }}>
-Experiments
+      <div className={useStyles.root}>
+        <Grid container spacing={3} style={{ marginTop: '150px', width: '2100px' }} justify="left" alignItems="left">
+          <Grid item xs={12}>
+            <Title>Browse Experiments data</Title>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography
+              align="left"
+              variant="body1"
+              style={{
+                fontWeight: 'normal', textUnderlinePosition: 'under', textDecorationStyle: 'initial', textDecoration: 'underline', textDecorationColor: red[500], color: 'black', borderBottomColor: red[500],
+              }}
+            > Experiments
+            </Typography>
+            <Divider />
+          </Grid>
+          <Grid item xs={12}>
+            <ExperimentsTable />
+          </Grid>
+        </Grid>
       </div>
     );
   }
 }
-export default ExperimentsComponent;
