e8ea644312b17368bc376d6354ce67932dbff402
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-27 13:48:38 -0400

    Add test mock up ui component in experiments

--- hubmap/frontend/src/pages/Experiments.js
+++ hubmap/frontend/src/pages/Experiments.js
@@ -2,12 +2,14 @@
 /* eslint-disable react/prefer-stateless-function */
 
 import React from 'react';
+import TestUIComponents from '../components/TestUIComponents';
 
 class ExperimentsComponent extends React.Component {
   render() {
     return (
       <div style={{ margin: 100 }}>
-Experiments
+        Experiments
+        <TestUIComponents />
       </div>
     );
   }
