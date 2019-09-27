4e65e1433800bab510a4ccd07ea79c7be0a9bdc2
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-27 13:47:58 -0400

    add Test mock up of ui components to demo and review, for feedback on PR 167

---
+++ hubmap/frontend/src/components/TestUIComponents.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import {
+  BrowseSummary, ControlledChipInput, SearchInput, BaseSelectDropdown, ReactAutosuggestExample,
+} from '../ui-components';
+import facets from '../data/searchfacets.json';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+    marginTop: '150px',
+    width: '2100px',
+    marginLeft: '-200px',
+  },
+}));
+const menuCategories = facets.facets.Assays;
+
+export default function MockupUIComponents() {
+  return (
+    <div className={useStyles.root}>
+      <Grid container spacing={3} style={{ marginTop: '150px', width: '2100px' }} justify="left" alignItems="left">
+        <Grid item xs={12}>
+            Browse Experiments data
+        </Grid>
+        <Grid item xs={3}>
+          <ControlledChipInput />
+        </Grid>
+        <Grid item xs={3}>
+          <SearchInput />
+        </Grid>
+        <Grid item xs={3}>
+          <BaseSelectDropdown
+            margin="normal"
+            variant="outlined"
+            menusections={menuCategories}
+            menuname="Assays"
+            width="200px"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <ReactAutosuggestExample />
+        </Grid>
+        <Grid item xs={12}>
+          <BrowseSummary />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
