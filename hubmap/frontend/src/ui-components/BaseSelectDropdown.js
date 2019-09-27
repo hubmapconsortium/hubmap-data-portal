1d89389dbcb33e88dbc3490148bdd09a5b962a19
Sushma Anand Akoju <sushma-ananda@users.noreply.github.com>
2019-09-27 15:46:23 -0400

    Update hubmap/frontend/src/ui-components/BaseSelectDropdown.js
    Co-Authored-By: Chuck McCallum <mccalluc@users.noreply.github.com>

--- hubmap/frontend/src/ui-components/BaseSelectDropdown.js
+++ hubmap/frontend/src/ui-components/BaseSelectDropdown.js
@@ -125,7 +125,6 @@ export default class CustomSelectMenu extends React.Component {
           variant="outlined"
         >
           {htmlElements}
-
         </Select>
       </MuiThemeProvider>
     );
