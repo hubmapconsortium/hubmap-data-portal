a968ace210fff9432f8dcf095410b414b84a3fc9
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-09-30 12:12:42 -0400

    Fix TODOs

--- hubmap/frontend/src/App.js
+++ hubmap/frontend/src/App.js
@@ -1,6 +1,4 @@
-// TODO!
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable max-len */
+// TODO:
 /* eslint-disable react/prop-types */
 
 import './App.scss';
@@ -9,27 +7,20 @@ import { connect } from 'react-redux';
 import RootContainer from './components/RootContainer';
 import { getTissueColorsFromServer, getAllExperiments, getGeneTissueColors } from './middleware/actions';
 
-// import '@google/model-viewer' ;
-// npm run setup -- --spaceId eo4e2dc0pbyt --deliveryToken H3bSZhVoA8_0_hjDzD6yGsq1jHCdBgxop3iJ9EM54B8 --managementToken CFPAT-nXzmTIQFv4Om1KFSnqn0fS3X7_3YLXDacst4IC52_1M
-/* TODO: add chart
-*   DONE: Add api get results fron django to populate results for each tissue
-*      DONE: Added organs prototype + clickable+rest api!! phew!! */
-
 
 class App extends PureComponent {
   componentDidMount() {
-    this.props.dispatch(getAllExperiments());
-    this.props.dispatch(getTissueColorsFromServer());
-    this.props.dispatch(getGeneTissueColors());
+    const { dispatch } = this.props;
+    dispatch(getAllExperiments());
+    dispatch(getTissueColorsFromServer());
+    dispatch(getGeneTissueColors());
   }
 
   render() {
     return (
       <div className="App">
-
         <RootContainer />
       </div>
-
     );
   }
 }
