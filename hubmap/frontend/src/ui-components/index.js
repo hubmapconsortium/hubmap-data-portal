d01fb0ce0d99809d2f64b1660c0fc59122d1ae1f
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-30 15:26:56 -0400

    remove exports of non-existent files

--- hubmap/frontend/src/ui-components/index.js
+++ hubmap/frontend/src/ui-components/index.js
@@ -2,7 +2,6 @@
  * This exports all required components for browse data page.
  */
 
-export { default as CustomSearchInput } from './CustomSearchInput';
 export { default as ControlledChipInput } from './ControlledChipInput';
 export { default as BaseSelectDropdown } from './BaseSelectDropdown';
 export { default as BrowseSummary } from './BrowseSummary';
