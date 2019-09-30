b943cc94e76679143da5815cd04d9fc61fd122eb
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-30 15:26:02 -0400

    remove SearchInput
    1) not used in any TestUIComponent/anywhere

--- hubmap/frontend/src/ui-components/index.js
+++ hubmap/frontend/src/ui-components/index.js
@@ -2,8 +2,7 @@
  * This exports all required components for browse data page.
  */
 
-export { default as SearchInput } from './SearchInput';
+export { default as CustomSearchInput } from './CustomSearchInput';
 export { default as ControlledChipInput } from './ControlledChipInput';
 export { default as BaseSelectDropdown } from './BaseSelectDropdown';
 export { default as BrowseSummary } from './BrowseSummary';
-export { default as ReactAutosuggestExample } from './AutoSuggest';
