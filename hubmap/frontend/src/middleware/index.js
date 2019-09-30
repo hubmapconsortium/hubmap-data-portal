30708c1b2a478e8d7ea076a352199a1c325b63bc
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-09-30 10:50:16 -0400

    rename PubSubApi to PubSub

--- hubmap/frontend/src/middleware/index.js
+++ hubmap/frontend/src/middleware/index.js
@@ -4,4 +4,4 @@
 
 export { default as rootReducer } from './rootReducer';
 export { default as store } from './store';
-export { default as PubSubApi } from './PubSub';
+export { default as PubSub } from './PubSub';
