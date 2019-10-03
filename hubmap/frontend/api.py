42989699d5ab41b11efceaf46039fdb6e650be06
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2019-10-03 12:31:09 -0400

    Filter donor uuids

--- hubmap/frontend/api.py
+++ hubmap/frontend/api.py
@@ -1,7 +1,15 @@
 import requests
 from urllib.parse import quote_plus
 
+# In general, unit tests should not depend on live servers
+# ... but in this case, it's all one project, and fairly simple,
+# and making sure docker works is actually a good thing, too.
+
+# Down the road, generate API client code from OpenAPI:
+# https://github.com/hubmapconsortium/hubmap-data-portal/issues/179
+
 base_url = 'http://localhost:9999/v0'
+
 
 def get_entity_types():
     '''
@@ -12,6 +20,7 @@ def get_entity_types():
     response = requests.get(f'{base_url}/entities').json()
     return response['entity_types']
 
+
 def get_entity(uuid):
     '''
     >>> entity = get_entity('11866f9586fdac34c95ac8a2737a83dd')
@@ -20,3 +29,24 @@ def get_entity(uuid):
     '''
     entity = requests.get(f'{base_url}/entities/{quote_plus(uuid)}').json()
     return entity
+
+
+def get_donor_uuids(user=None):
+    '''
+    >>> donor_uuids = get_donor_uuids()
+    >>> all([type(id) is str for id in donor_uuids])
+    True
+    >>> all([len(id) == 32 for id in donor_uuids])
+    True
+
+    >>> filtered_uuids = get_donor_uuids(user='shirey@pitt.edu')
+    >>> all([type(id) is str for id in filtered_uuids])
+    True
+    >>> all([len(id) == 32 for id in filtered_uuids])
+    True
+    '''
+    if user:
+        response = requests.get(f'{base_url}/entities/donors/created-by/{user}').json()
+    else:
+        response = requests.get(f'{base_url}/entities/donors').json()
+    return response['uuids']
