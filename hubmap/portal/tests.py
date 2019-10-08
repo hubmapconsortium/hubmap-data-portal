c736e89c959ec18ab95873566e4231695a38db13
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-10-08 16:52:11 -0400

    add tests for portal/views.py

--- hubmap/portal/tests.py
+++ hubmap/portal/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .views import listall, entities, details, index
+from django.test import RequestFactory
+
+
+class PortalViewsTestCase(TestCase):
+    def setUp(self) -> None:
+        self.factory = RequestFactory()
+        self.request = self.factory.request()
+        self.entity_type = 'donor'
+        self.hubmap_id = 4
+        pass
+
+    def test_home_page(self):
+        response = index(self.request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.content)
+        pass
+
+    def test_list_entities_page(self):
+        response = listall(self.request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.content)
+        pass
+
+    def test_entities_by_type_page(self):
+        response = entities(self.request, self.entity_type)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.content)
+        pass
+
+    def test_browse_entity_detail_page(self):
+        response = details(self.request, self.entity_type, self.hubmap_id)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.content)
+        pass
