d41dcd0f2bb215e39d92f672e3ec9cc6225fea76
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-10-08 16:45:37 -0400

    remove unused imports, reformat file

--- hubmap/portal/views.py
+++ hubmap/portal/views.py
@@ -1,14 +1,13 @@
-import json
 import string
-from collections import OrderedDict
-
 from django.shortcuts import render
-entities_list = [{"entity_type": "sample", "hubmap_id": 1 },
- {"entity_type": "sample", "hubmap_id": 2},
- {"entity_type": "donor", "hubmap_id": 3},
- {"entity_type": "donor", "hubmap_id": 4},
- {"entity_type": "dataset", "hubmap_id": 5},
- {"entity_type": "dataset", "hubmap_id": 6}]
+
+entities_list = [{"entity_type": "sample", "hubmap_id": 1},
+                 {"entity_type": "sample", "hubmap_id": 2},
+                 {"entity_type": "donor", "hubmap_id": 3},
+                 {"entity_type": "donor", "hubmap_id": 4},
+                 {"entity_type": "dataset", "hubmap_id": 5},
+                 {"entity_type": "dataset", "hubmap_id": 6}]
+
 
 # Create your views here.
 def index(request):
@@ -17,7 +16,7 @@ def index(request):
     :param request:
     :return:
     """
-    return render(request, 'portal_home.html', {"title":"HuBMAP"})
+    return render(request, 'portal_home.html', {"title": "HuBMAP"})
 
 
 def details(request, entity_type: string, hubmap_id: int):
@@ -26,15 +25,13 @@ def details(request, entity_type: string, hubmap_id: int):
         'details.html',
         {
             "entity_type": entity_type,
-            "hubmap_id":hubmap_id,
+            "hubmap_id": hubmap_id,
             "title": "Browse Entity Details"
         }
     )
 
 
 def listall(request):
-    print(entities_list)
-    print(type(entities_list))
     return render(
         request,
         'listall.html',
@@ -44,16 +41,18 @@ def listall(request):
         }
     )
 
-def entities(request, entity_type:string):
-    filtered_list = [ entity for entity in entities_list if entity['entity_type'].__eq__( entity_type)]
-    print( filtered_list)
+
+def entities(request, entity_type: string):
+    filtered_list = [entity for entity in entities_list
+                     if entity['entity_type'].__eq__(entity_type)]
     return render(request,
-           'entities.html',
-           {
-               "filtered_list": filtered_list,
-               "entity_type": entity_type,
-               "title": "Browse Entities of " + entity_type
-           })
+                  'entities.html',
+                  {
+                      "filtered_list": filtered_list,
+                      "entity_type": entity_type,
+                      "title": "Browse Entities of " + entity_type
+                  })
+
 
 def help(request):
     return render(request, 'help.html', {"title": "Help"})
