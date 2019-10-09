d5481e1c52f4393715b62ca576fc37d6b22b51a0
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-10-09 16:48:45 -0400

    add trailing comma travis build error fix (nor replicable on local test runs )

--- hubmap/portal/urls.py
+++ hubmap/portal/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('all/', views.listall, name='listall'),
     path('browse/<entity_type>/<int:hubmap_id>', views.details, name='details'),
     path('browse/<entity_type>', views.entities, name='entities'),
-    path('help', views.help, name='help')
+    path('help', views.help, name='help'),
 ]
