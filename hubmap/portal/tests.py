3cb72606076f2b62fc6e1327a9af11662890f508
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-10-10 10:32:14 -0400

    remove extra pass PR 203 (RC by Matt)

--- hubmap/portal/tests.py
+++ hubmap/portal/tests.py
@@ -1,7 +1,7 @@
 from django.test import RequestFactory, TestCase
 
 # Create your tests here.
-from .views import details, entities, index, listall
+from .views import details, entities, index, list_all
 
 
 class PortalViewsTestCase(TestCase):
@@ -10,7 +10,6 @@ class PortalViewsTestCase(TestCase):
         self.request = self.factory.request()
         self.entity_type = 'donor'
         self.hubmap_id = 4
-        pass
 
     def test_home_page(self):
         response = index(self.request)
@@ -19,19 +18,16 @@ class PortalViewsTestCase(TestCase):
         pass
 
     def test_list_entities_page(self):
-        response = listall(self.request)
+        response = list_all(self.request)
         self.assertEqual(response.status_code, 200)
         self.assertIsNotNone(response.content)
-        pass
 
     def test_entities_by_type_page(self):
         response = entities(self.request, self.entity_type)
         self.assertEqual(response.status_code, 200)
         self.assertIsNotNone(response.content)
-        pass
 
     def test_browse_entity_detail_page(self):
         response = details(self.request, self.entity_type, self.hubmap_id)
         self.assertEqual(response.status_code, 200)
         self.assertIsNotNone(response.content)
-        pass
