b2a7df2e8432d2473537b5b813a8b63760502d29
Sushma Anand Akoju <sakoju@andrew.cmu.edu>
2019-10-10 10:31:23 -0400

    add portal urls here as templates are rendered from frontend PR203 (RC by Matt)

--- hubmap/frontend/urls.py
+++ hubmap/frontend/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.FrontendAppView.as_view(), name='index'),
     path(settings.LOGIN_REDIRECT_URL.lstrip('/'), views.set_user_cookie, name='set-user-cookie'),
     path('logout/', views.logout, name='logout'),
+    path('portal/', include('portal.urls')),
 ]
